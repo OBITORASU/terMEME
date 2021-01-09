04dda1f3e70af81deef74650d6dc20e858432ef9
Souhit Dey <65222459+OBITORASU@users.noreply.github.com>
2021-01-09 23:50:39 +0530

    Update meme.py

--- meme.py
+++ meme.py
@@ -2,20 +2,25 @@ import json
 import requests
 import time
 
-
+# Get a list of image url from a supplied subreddit and category 
 def get_meme(str1, str2):
+    # Big number for major shenanigans 
     number = 99999
     url = "https://www.reddit.com/r/{}/{}/".format(str1,str2)
+    # Request to fetch .json data
     r = requests.get("{}.json?limit={}".format(url, number), headers = {'user-agent': 'Mozilla/5.0'})
     
+    # List to store urls and the following one to validate file format
     meme_list = []
     formats = ["jpeg", "jpg", "png"]
     
+    # Loop through all url and validate against permitted formats
     for post in r.json()['data']['children']:
         
         if ([ele for ele in formats if(ele in post['data']['url'])]):
             meme_list.append(post['data']['url'])
-
+    
+    # Return the list of image urls
     return (meme_list)
 
 
