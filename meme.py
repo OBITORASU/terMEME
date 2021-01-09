41a2594f7a5d5a0acf8aee64d1ce543dc2571a5a
Souhit Dey <65222459+OBITORASU@users.noreply.github.com>
2021-01-09 17:12:23 +0530

    Update meme.py

--- meme.py
+++ meme.py
@@ -3,18 +3,27 @@ import requests
 import time
 
 
-
+# A function to scrape memes from r/dankmemes with a string parameter 
 def get_meme(str):
-    number = 99999
-    url = "https://www.reddit.com/r/dankmemes/{}/".format(str)
-    r = requests.get("{}.json?limit={}".format(url, number), headers = {'user-agent': 'Mozilla/5.0'})
     
+    # A big number so you dont run out of dankness
+    number = 99999
+    # url to scrape from
+    url = "https://www.reddit.com/r/dankmemes/{}/".format(str)
+    
+    # Using requests module to scrape the .json data
+    r = requests.get("{}.json?limit={}".format(url, number), headers = {'user-agent': 'Mozilla/5.0'})
+    # An empty list to store all the shenanigans
     meme_list = []
-
+    
+    # Loop through the list
     for post in r.json()['data']['children']:
+        
         if "gif" not in post['data']['url']:
+            # Get the url of the meme 
             meme_list.append(post['data']['url'])
-
+    
+    # Return the list populated list of meme url
     return (meme_list)
 
 
